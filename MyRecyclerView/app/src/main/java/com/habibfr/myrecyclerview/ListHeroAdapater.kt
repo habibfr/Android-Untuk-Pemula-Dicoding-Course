c7c37b39e8f58e4b0e17cfa23e0f79666c42c26f
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-02-27 21:42:50 +0700

    first make recycleview

---
+++ MyRecyclerView/app/src/main/java/com/habibfr/myrecyclerview/ListHeroAdapater.kt
@@ -0,0 +1,35 @@
+package com.habibfr.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapater(private val listHero: ArrayList<Hero>) :
+    RecyclerView.Adapter<ListHeroAdapater.ListViewHolder>() {
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.imgItemPhoto)
+        val txtName: TextView = itemView.findViewById(R.id.txtItemName)
+        val txtDescription: TextView = itemView.findViewById(R.id.txtItemDescription)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+
+        holder.imgPhoto.setImageResource(photo)
+        holder.txtName.text = name
+        holder.txtDescription.text = description
+    }
+}
---
+++ MyRecyclerView/app/src/main/java/com/habibfr/myrecyclerview/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.habibfr.myrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHero: RecyclerView
+    private val list = ArrayList<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHero = findViewById(R.id.rvHero)
+        rvHero.setHasFixedSize(true)
+
+        list.addAll(getListHero())
+        showRecyclerList()
+    }
+
+    private fun getListHero(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listHero = ArrayList<Hero>()
+
+        for (i in dataName.indices) {
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rvHero.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapater = ListHeroAdapater(list)
+        rvHero.adapter = listHeroAdapater
+    }
+}
