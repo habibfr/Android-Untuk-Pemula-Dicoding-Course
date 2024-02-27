aca85baffe3a9d2208eb61b3abd5f6c753c09099
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-02-27 21:47:10 +0700

    add onclick on adapter

--- MyRecyclerView/app/src/main/java/com/habibfr/myrecyclerview/ListHeroAdapater.kt
+++ MyRecyclerView/app/src/main/java/com/habibfr/myrecyclerview/ListHeroAdapater.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ListHeroAdapater(private val listHero: ArrayList<Hero>) :
@@ -31,5 +32,13 @@ class ListHeroAdapater(private val listHero: ArrayList<Hero>) :
         holder.imgPhoto.setImageResource(photo)
         holder.txtName.text = name
         holder.txtDescription.text = description
+
+        holder.itemView.setOnClickListener() {
+            Toast.makeText(
+                holder.itemView.context,
+                "Kamu memilih " + listHero[holder.adapterPosition].name,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
