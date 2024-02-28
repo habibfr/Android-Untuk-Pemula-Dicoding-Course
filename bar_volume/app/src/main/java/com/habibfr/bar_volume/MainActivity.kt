b2919c941e8c722ebb5565fbc0e0cf0e3ecc500d
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-02-28 14:05:21 +0700

    add a view binding

--- bar_volume/app/src/main/java/com/habibfr/bar_volume/MainActivity.kt
+++ bar_volume/app/src/main/java/com/habibfr/bar_volume/MainActivity.kt
@@ -6,68 +6,76 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.habibfr.bar_volume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var etPanjang: EditText;
-    private lateinit var etLebar: EditText;
-    private lateinit var etTinggi: EditText;
-    private lateinit var btnHitung: Button;
-    private lateinit var txtHasil: TextView;
+//    private lateinit var etPanjang: EditText;
+//    private lateinit var etLebar: EditText;
+//    private lateinit var etTinggi: EditText;
+//    private lateinit var btnHitung: Button;
+//    private lateinit var txtHasil: TextView;
 
     companion object {
         private const val STATE_RESULT = "state_result"
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//
+//        etPanjang = findViewById(R.id.etPanjang);
+//        etLebar = findViewById(R.id.etLebar);
+//        etTinggi = findViewById(R.id.etTinggi);
+//        btnHitung = findViewById(R.id.btnHitung)
+//        txtHasil = findViewById(R.id.txtHasil)
+//
+//        btnHitung.setOnClickListener(this)
 
-        etPanjang = findViewById(R.id.etPanjang);
-        etLebar = findViewById(R.id.etLebar);
-        etTinggi = findViewById(R.id.etTinggi);
-        btnHitung = findViewById(R.id.btnHitung)
-        txtHasil = findViewById(R.id.txtHasil)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnHitung.setOnClickListener(this)
+        binding.btnHitung.setOnClickListener(this)
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT)
-            txtHasil.text = result
+            binding.txtHasil.text = result
         }
     }
 
     override fun onClick(p0: View?) {
         if (p0?.id == R.id.btnHitung) {
-            val panjang = etPanjang.text.toString().trim()
-            val lebar = etLebar.text.toString().trim()
-            val tinggi = etTinggi.text.toString().trim()
+            val panjang = binding.etPanjang.text.toString().trim()
+            val lebar = binding.etLebar.text.toString().trim()
+            val tinggi = binding.etTinggi.text.toString().trim()
 
 
             var isValueEmpty = false
 
             if (panjang.isEmpty()) {
                 isValueEmpty = true
-                etPanjang.error = "Field panjang tidak boleh kosong!"
+                binding.etPanjang.error = "Field panjang tidak boleh kosong!"
             }
             if (lebar.isEmpty()) {
                 isValueEmpty = true
-                etLebar.error = "Field lebar tidak boleh kosong!"
+                binding.etLebar.error = "Field lebar tidak boleh kosong!"
             }
             if (panjang.isEmpty()) {
                 isValueEmpty = true
-                etTinggi.error = "Field tinggi tidak boleh kosong!"
+                binding.etTinggi.error = "Field tinggi tidak boleh kosong!"
             }
 
             if (!isValueEmpty) {
                 val hasil = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
-                txtHasil.text = hasil.toString()
+                binding.txtHasil.text = hasil.toString()
             }
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, txtHasil.text.toString())
+        outState.putString(STATE_RESULT, binding.txtHasil.text.toString())
     }
 }
